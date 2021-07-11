f2752f3b034b6c1e5b22892de3ccef4a7494cce0
rainbow1324 <mdk2435@gmail.com>
2021-07-11 12:49:50 +0900

    7.11

---
+++ practice1/src/practice1/aaa.java
@@ -0,0 +1,109 @@
+package practice1;
+
+import java.util.Scanner;
+
+public class aaa {
+
+	public static void main(String[] args) {
+		
+		
+		
+		
+		for(int i=0;i<10;i++) {
+			
+			System.out.print("*");
+			System.out.println();
+		}
+		
+		
+		
+		
+		
+		
+		
+		
+		
+		
+		
+		
+		
+		
+		
+		
+		
+		
+		
+		
+		
+		
+		
+		
+		
+		
+		
+		
+		
+		
+		
+		
+		
+//		for(int i=5;i>=1;i--){
+//			for(int j=1;j<=i;j++){
+//				System.out.print("*");
+//			}
+//			System.out.println("");
+//		}
+//		for(int i=2;i<=5;i++){
+//			for(int j=1;j<=i;j++){
+//				System.out.print("*");
+//			}
+//			System.out.println("");
+//		}
+//		
+		
+		
+		
+		
+//		for(int i=0;i<3;i++){
+//			for(int j=0;j<4;j++){
+//				System.out.print("*");
+//			}
+//			System.out.println("");
+//		}
+//		
+		
+		
+		
+		
+		
+		
+		
+		
+		
+	}
+}
+	
+		
+		
+		
+		
+		
+		
+		
+		
+		
+		
+
+	
+	
+
+
+	
+
+
+
+
+
+
+
+
